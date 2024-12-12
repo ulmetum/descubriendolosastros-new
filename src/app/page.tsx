83670714e55cae8b906a60b94363f853f1abcc8b
ulmetum <ulmetum@gmail.com>
2024-12-12 20:05:45 +0100

    Añadido metadatos para la pagina de Inicio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import { SectionFeatures, SectionIntro } from '@/components'
 import SectionServices from '@/components/sections/sectionServices'
 
+export const metadata = {
+  title: 'Descubriendo los astros | Inicio',
+  description:
+    'Explora el fascinante universo de la astronomía y la astrología en descubriendolosastros.com. Aprende sobre constelaciones, planetas, fenómenos cósmicos y cómo los astros influyen en nuestras vidas. Una mirada al cielo para descubrir su magia y ciencia.',
+}
+
 export default async function HomePage() {
   return (
     <>
