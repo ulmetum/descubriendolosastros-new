8628fa6d0092b368ab5427d5da4156286b62041e
ulmetum <ulmetum@gmail.com>
2024-12-17 17:16:53 +0100

    Reparado problema con la imagen seo en la pagina de blog

--- src/app/blog/[[...page]]/page.tsx
+++ src/app/blog/[[...page]]/page.tsx
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
     siteName: 'Descubriendo los astros',
     images: [
       {
-        url: 'https://descubriendolosastros.com/opengraph-image.jpg',
+        url: 'https://descubriendolosastros.com/blog/opengraph-image.jpg',
         width: 1200,
         height: 630,
         alt: 'Descubriendo los astros',
