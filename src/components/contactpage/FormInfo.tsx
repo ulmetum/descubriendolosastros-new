d6b94f31e2e15ee35990661318118df466de6cfb
ulmetum <ulmetum@gmail.com>
2024-12-23 18:58:28 +0100

    Nuevo texto informativo en la pagina de contacto

--- src/components/contactpage/FormInfo.tsx
+++ src/components/contactpage/FormInfo.tsx
@@ -12,45 +12,44 @@ export const FormInfo = () => {
       className='bg-white px-4 lg:px-8 py-20 w-[min(100%,1024px)] lg:mx-auto lg:-mt-[10rem] relative z-10 mb-20 lg:rounded-xl lg:shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px]'
     >
       <p className='text-xl sm:text-2xl'>
-        Si quieres conocer qué mensaje tienen para ti las estrellas y develar el
-        camino trazado por los astros, puedes ponerte en contacto conmigo a
-        través del formulario que verás más abajo o a través del correo
-        electrónico:
+        Si quieres conocer cómo era el cielo en ese momento tan especial, o
+        quieres conocer qué mensajes tienen para ti las estrellas puedes
+        solicitar tu Mapa Estelar o tu Carta Astral contactando conmigo a través
+        del formulario que verás a continuación:
       </p>
-      <div className='my-10 sm:my-16'>
+      {/* <div className='my-10 sm:my-16'>
         <CopyClipboard color='amber' />
-      </div>
+      </div> */}
 
       <p className='mt-6 text-xl sm:text-2xl'>
-        Cuando tenga los datos, previo pago, procederé a elaborar el mapa
-        personalizado.{' '}
-        <span className='font-semibold sm:text-2xl underline-offset-2 underline'>
-          Una vez procesado el pedido, no habrá devolución
-        </span>
-        .
-      </p>
-      <p className='mt-6 text-xl font-semibold sm:text-2xl'>
-        El tiempo aproximado de elaboración es de 7 días para formato digital y
-        de 15 días para formato{' '}
+        Una vez que reciba yo los datos,{' '}
         <span className='mt-6 text-xl font-semibold sm:text-2xl relative'>
-          físico{' '}
-          <span className='font-semibold text-base absolute -top-1 -right-1'>
+          previo pago
+          <span className='font-semibold text-base absolute -top-1 -right-2'>
             *
           </span>
-        </span>{' '}
-        .
-      </p>
-      <p className='mt-6 text-xl font-semibold sm:text-xl'>
-        <span className='font-semibold text-lg'>*</span> Los pedidos en formato
-        físico solo se podrán enviar a{' '}
-        <span className=' font-semibold underline underline-offset-2'>
-          España
         </span>
-        .
+        , procederé a elaborar el producto.
       </p>
-      <p className='mt-6 text-xl font-semibold sm:text-xl'>
-        <span className='font-semibold text-lg'>**</span> Debes indicar la hora
-        del evento junto a la fecha del evento.
+
+      <p className='mt-6 text-xl font-semibold sm:text-2xl'>
+        <span className='font-semibold text-lg'>*</span> Al tratarse de
+        productos digitales y personalizados, una vez realizado el pago, no
+        habrá devolución, según las leyes de consumo de la UE.
+      </p>
+      <p className='mt-6 text-xl font-semibold sm:text-2xl'>
+        <span className='font-semibold text-lg'>**</span> El Mapa Estelar y/o la
+        Carta Astral se entragarán en un periodo de 7 días (el Mapa Estelar) a
+        15 días (la Carta Astral) a la dirección de correo electrónico
+        facilitada.
+      </p>
+      <p className='mt-6 text-xl sm:text-2xl'>
+        Por favor, es muy importante que rellenes correctamente todos los
+        campos.
+      </p>
+      <p className='mt-6 text-xl sm:text-2xl'>
+        Si necesitas comunicarme algo tienes la opción de escribirme tu mensaje
+        en el caja de comentario del formulario y me pondré en contacto contigo.
       </p>
     </motion.div>
   )
