cc9753ae602df8333e22f201d688388a64d090a4
ulmetum <ulmetum@gmail.com>
2025-02-12 16:41:26 +0100

    Añadida configuracion de Brevo

---
+++ src/utils/brevo.ts
@@ -0,0 +1,51 @@
+import { API_KEY_BREVO } from '@/config'
+import * as brevo from '@getbrevo/brevo'
+
+const apiInstance = new brevo.TransactionalEmailsApi()
+
+apiInstance.setApiKey(
+  brevo.TransactionalEmailsApiApiKeys.apiKey,
+  API_KEY_BREVO as string
+)
+
+// interface Props {
+//   to: { email: string; name: string }[]
+//   subject: string
+//   htmlContent: string
+// }
+
+interface Props {
+  to: { email: string; name: string }[]
+  templateId: number
+  subject?: string
+  params: Record<string, any>
+}
+
+export const sendEmailContact = async ({
+  to,
+  templateId,
+  params,
+  subject,
+}: Props) => {
+  // export const sendEmail = async ({ to, subject, htmlContent }: Props) => {
+  try {
+    const smtpEmail = new brevo.SendSmtpEmail()
+
+    // smtpEmail.htmlContent = `<html><body><div>${htmlContent}</div></body></html>`
+    smtpEmail.subject = subject
+    smtpEmail.templateId = templateId
+    smtpEmail.to = to
+    smtpEmail.params = params
+    smtpEmail.sender = {
+      email: 'hola@descubriendolosastros.com',
+      name: 'Mirova',
+    }
+    const response = await apiInstance.sendTransacEmail(smtpEmail)
+
+    console.log('Correo enviado correctamente:', response)
+
+    return response
+  } catch (error) {
+    console.error(error)
+  }
+}
