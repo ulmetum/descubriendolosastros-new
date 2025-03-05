3bc6356e1407a36bcd6b89bd7f80bdc8c8a25bb8
ulmetum <ulmetum@gmail.com>
2025-03-05 16:09:24 +0100

    Modificacion de la animacion de entrada del componente FormInfo.tsx

--- src/components/contactpage/FormInfo.tsx
+++ src/components/contactpage/FormInfo.tsx
@@ -12,7 +12,7 @@ export const FormInfo = ({ children }: Props) => {
     <motion.div
       initial={{ opacity: 0, y: 50 }}
       animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 1.5, delay: 1.5 }}
+      transition={{ duration: 0.5, delay: 0.75 }}
       className='bg-white px-4 lg:px-8 py-20 w-[min(100%,1024px)] lg:mx-auto lg:-mt-[10rem] relative z-10 mb-20 lg:rounded-xl lg:shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px]'
     >
       {children}
