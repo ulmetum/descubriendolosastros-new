60254bfebbc6cd1041a51d0b559511f3962a2b81
ulmetum <ulmetum@gmail.com>
2025-03-05 16:01:54 +0100

    Actualizada configuracion de stripe en el archivo stripe.ts

--- src/utils/stripe.ts
+++ src/utils/stripe.ts
@@ -2,6 +2,6 @@ import { STRIPE_SECRET_KEY } from '@/config'
 import Stripe from 'stripe'
 
 export const stripe = new Stripe(STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-01-27.acacia',
+  apiVersion: '2025-02-24.acacia',
   typescript: true,
 })
